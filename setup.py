263f84197cd6728837123a8ceb844cb473064c34
He <z.he@iek13006.iek.kfa-juelich.de>
2020-02-21 10:41:50 +0100

    Add .readthedocs.yml and requirements.txt

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ author='Zheng-Da He',
 author_email='z.he@fz-juelich.de, jameshzd@mail.ustc.edu.cn, zhengdahe.electrocatalysis@gmail.com',
 license='MIT',
 packages=setuptools.find_packages(),
+requirements=['ase>=3.19.0', 'pymatgen>=2020.1.28']
 zip_safe=False)
--- source/conf.py
+++ source/conf.py
@@ -12,11 +12,12 @@
 #
 import os
 import sys
+import surfgen 
+
 sys.path.insert(0, os.path.abspath('../surfgen'))
 
 def setup(app):
     app.add_stylesheet('my_theme.css')
-
 
 # -- Project information -----------------------------------------------------
 
